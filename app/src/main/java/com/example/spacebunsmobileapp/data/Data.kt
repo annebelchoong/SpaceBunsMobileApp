20641bfde2e7ae207042340912cecdf2f151b46b
Annebel Choong <annebelchoong@gmail.com>
2022-10-04 02:44:15 +0800

    Add cart and customer data

--- app/src/main/java/com/example/spacebunsmobileapp/data/Data.kt
+++ app/src/main/java/com/example/spacebunsmobileapp/data/Data.kt
@@ -2,6 +2,7 @@ package com.example.spacebunsmobileapp.data
 
 import com.google.firebase.firestore.Blob
 import com.google.firebase.firestore.DocumentId
+import com.google.firebase.firestore.Exclude
 import com.google.firebase.firestore.ktx.firestore
 import com.google.firebase.ktx.Firebase
 import java.util.*
@@ -21,4 +22,22 @@ data class Product(
     var photo: Blob = Blob.fromBytes(ByteArray(0)),
 )
 
+data class Cart(
+    @DocumentId
+    var productId: String = "",
+    var productName: String = "",
+    var quantity: Int = 0,
+    var price: Double = 0.00,
+){
+    @get:Exclude
+    var totalPrice: Double = 0.0
+}
+
+data class User(
+    @DocumentId
+    var customerId: String = ""
+)
+
 val PRODUCTS = Firebase.firestore.collection("products")
+val CART = Firebase.firestore.collection("usersTest").document("U001").collection("cart")
+val CUST = Firebase.firestore.collection("usersTest")
