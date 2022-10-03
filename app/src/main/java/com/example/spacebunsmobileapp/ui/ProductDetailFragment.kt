75dd0da131a97d50632d8df485ed9d8549d9e801
Annebel Choong <annebelchoong@gmail.com>
2022-10-04 02:45:53 +0800

    get the product detail displayed and add item to cart

--- app/src/main/java/com/example/spacebunsmobileapp/ui/ProductDetailFragment.kt
+++ app/src/main/java/com/example/spacebunsmobileapp/ui/ProductDetailFragment.kt
@@ -9,10 +9,13 @@ import androidx.fragment.app.activityViewModels
 import androidx.lifecycle.lifecycleScope
 import androidx.navigation.fragment.findNavController
 import com.example.spacebunsmobileapp.R
+import com.example.spacebunsmobileapp.data.CART
+import com.example.spacebunsmobileapp.data.Cart
 import com.example.spacebunsmobileapp.data.ProductViewModel
 import com.example.spacebunsmobileapp.databinding.FragmentHomeBinding
 import com.example.spacebunsmobileapp.databinding.FragmentProductDetailBinding
 import com.example.spacebunsmobileapp.util.setImageBlob
+import com.google.android.material.snackbar.Snackbar
 import kotlinx.coroutines.launch
 
 class ProductDetailFragment : Fragment() {
@@ -23,6 +26,7 @@ class ProductDetailFragment : Fragment() {
     private val id by lazy {arguments?.getString("id","")?: ""}
 
 
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         binding = FragmentProductDetailBinding.inflate(inflater, container, false)
 
@@ -31,13 +35,31 @@ class ProductDetailFragment : Fragment() {
             binding.imageView3.setImageBlob(product.photo)
             binding.txtProductName.text = product.name
             binding.txtDesc.text = product.desc
+            vm.productId = product.productId
+            vm.custId = "U001"
         }
 
         binding.btnAddToCart.setOnClickListener {
-            nav.navigateUp()
-
+            addToCart()
+            Snackbar.make(binding.root, "Product Added to Cart!", Snackbar.LENGTH_SHORT).show()
         }
 
         return binding.root
+    }
+
+
+    private fun addToCart() {
+            val c = Cart()
+
+            c.productId = vm.productId
+            c.productName = binding.txtProductName.text.toString()
+            c.quantity = binding.txtQuantity.text.toString().toIntOrNull() ?: 1
+            c.price = binding.txtTotalPrice.text.toString().toDoubleOrNull() ?: 0.00
+            c.totalPrice = c.quantity * c.price
+
+            vm.setCart(c)
+
+            nav.navigateUp()
+
     }
 }
