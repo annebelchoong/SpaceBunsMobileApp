b38d88667c4d7c468d333610d6ba7beb19e2b3ba
annebelchoong <80569866+annebelchoong@users.noreply.github.com>
2022-10-05 13:47:40 +0800

    Revert "Merge login_auth to master"

--- app/src/main/java/com/example/spacebunsmobileapp/ui/DonationFragment.kt
+++ app/src/main/java/com/example/spacebunsmobileapp/ui/DonationFragment.kt
@@ -5,25 +5,56 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.navigation.fragment.findNavController
 import com.example.spacebunsmobileapp.R
-import com.example.spacebunsmobileapp.databinding.FragmentDonationBinding
 
-class DonationFragment : Fragment() {
-    private lateinit var binding: FragmentDonationBinding
-    private val nav by lazy { findNavController() }
+// TODO: Rename parameter arguments, choose names that match
+// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
+private const val ARG_PARAM1 = "param1"
+private const val ARG_PARAM2 = "param2"
 
+/**
+ * A simple [Fragment] subclass.
+ * Use the [RewardFragment.newInstance] factory method to
+ * create an instance of this fragment.
+ */
+class RewardFragment : Fragment() {
+    // TODO: Rename and change types of parameters
+    private var param1: String? = null
+    private var param2: String? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        arguments?.let {
+            param1 = it.getString(ARG_PARAM1)
+            param2 = it.getString(ARG_PARAM2)
+        }
+    }
 
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View {
-        binding = FragmentDonationBinding.inflate(inflater, container, false)
-        binding.btnCheckoutTest.setOnClickListener { nav.navigate(R.id.checkoutFragment) }
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_donation, container, false)
+    }
 
-//        PaymentConfiguration.init(applicationContext,resources.getString(R.string.stripe_publishable_key))
-
-        return binding.root
+    companion object {
+        /**
+         * Use this factory method to create a new instance of
+         * this fragment using the provided parameters.
+         *
+         * @param param1 Parameter 1.
+         * @param param2 Parameter 2.
+         * @return A new instance of fragment RewardFragment.
+         */
+        // TODO: Rename and change types and number of parameters
+        @JvmStatic
+        fun newInstance(param1: String, param2: String) =
+            RewardFragment().apply {
+                arguments = Bundle().apply {
+                    putString(ARG_PARAM1, param1)
+                    putString(ARG_PARAM2, param2)
+                }
+            }
     }
 }
--- app/src/main/java/com/example/spacebunsmobileapp/util/TemplateUtil.kt
+++ app/src/main/java/com/example/spacebunsmobileapp/util/TemplateUtil.kt
@@ -1,6 +1,4 @@
 package com.example.spacebunsmobileapp.util
 
-
 class TemplateUtil {
 }
-
